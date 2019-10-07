112da5171c885179c117a210cdb5b7d728bfafa5
sreedhar93 <marujollasreedhar123@gmail.com>
2019-10-07 16:23:42 +0530

    latest

---
+++ src/main/java/java/project/App.java
@@ -0,0 +1,10 @@
+package java.project;
+
+public class App {
+	public String sample () {
+		return "sample";
+		
+	}
+
+}
+
---
+++ src/test/java/java/project/Apptest.java
@@ -0,0 +1,14 @@
+package java.project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Apptest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
